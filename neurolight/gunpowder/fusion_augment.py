2c06899f4310514b71ad0cd5a687872847ccf473
maisli <mali@posteo.net>
2019-02-25 19:41:30 +0100

    alpha blending for intensities

--- neurolight/gunpowder/fusion_augment.py
+++ neurolight/gunpowder/fusion_augment.py
@@ -1,23 +1,27 @@
 import numpy as np
 from gunpowder import *
-import copy
 from scipy import ndimage
 
 
-class FusionAugment(BatchProvider): #todo: use Fusion Augment as BatchFilter
+class FusionAugment(BatchFilter):
     """Combine foreground of one or more volumes with another using soft mask and convex combination.
         Args:
-            ch1 (:class:``ArrayKey``):
+            arrays (``list`` of :class:``ArrayKey``):
 
-                The intensity array to modify. Should be of type float and within range [-1, 1] or [0, 1].
-
-            ch2 (:class:``ArrayKey``):
-
-                The intensity array to modify. Should be of type float and within range [-1, 1] or [0, 1].
+                The intensity arrays to modify.
 
             gt (:class:``ArrayKey``):
 
                 The labeled array to use as mask to cut out foreground objects.
+
+            alpha_blending_ref(``string``, optional):
+
+                Determines if alpha blending should be applied to intensities of volume B or gt mask of it.
+
+            num_blended_obj (``int``):
+
+                The number of objects which should be used from volume B. Use 0 to copy all objects. Can only be \
+                applied to alpha_blending_ref gt.
 
             smoothness (``float``, optional):
 
@@ -28,131 +32,152 @@ class FusionAugment(BatchProvider): #todo: use Fusion Augment as BatchFilter
                 If intermediate results should be returned (ch1 and ch2 of the two merged volumes (A and B) + soft mask)
     """
 
-    def __init__(self, ch1, ch2, gt, smoothness=3, return_intermediate=False):
+    def __init__(self, arrays, gt, alpha_blending_ref='intensity', num_blended_obj=0, smoothness=3,
+                 return_intermediate=False):
 
-        self.ch1 = ch1
-        self.ch2 = ch2
+        if type(arrays) in [list, tuple]:
+            self.arrays = list(arrays)
+        else:
+            self.arrays = [arrays, ]
+
         self.gt = gt
-        self.gt_spec = None
-        self.ch1_spec = None
+        self.gt_roi = None
+        self.array_roi = None
+        self.alpha_blending_ref = alpha_blending_ref
+        self.num_blended_obj = num_blended_obj
         self.smoothness = smoothness
-
         self.intermediates = None
+
         if return_intermediate:
 
-            self.intermediates = (
-                ArrayKey('A_' + self.ch1.identifier),
-                ArrayKey('A_' + self.ch2.identifier),
-                ArrayKey('B_' + self.ch1.identifier),
-                ArrayKey('B_' + self.ch2.identifier),
-                ArrayKey('SOFT_MASK')
-            )
+            self.intermediates = []
+
+            for array in self.arrays:
+                self.intermediates += [
+                    ArrayKey('A_' + array.identifier),
+                    ArrayKey('B_' + array.identifier),
+                ]
+            if self.alpha_blending_ref == 'gt':
+                self.intermediates += [ArrayKey('SOFT_MASK'),]
+
+        assert self.alpha_blending_ref in ['intensity', 'gt'], (
+                "Unknown alpha blending reference %s." % self.alpha_blending_ref)
 
     def setup(self):
 
-        assert len(
-            self.get_upstream_providers()) > 1, "at least two batch provider needs to be added to the FusionAugment"
-
-        common_spec = None
-
-        # advertise outputs only if all upstream providers have them
-        for provider in self.get_upstream_providers():
-
-            if common_spec is None:
-                common_spec = copy.deepcopy(provider.spec)
-            else:
-                for key, spec in provider.spec.items():
-                    if key not in common_spec:
-                        del common_spec[key]
-
-        for key, spec in common_spec.items():
-            self.provides(key, spec)
+        if len(self.arrays) > 1:
+            for i in range(1, len(self.arrays)):
+                assert(self.spec[self.arrays[0]] == self.spec[self.arrays[i]], (
+                    "Specs for %s and %s are not the same. Please check!"
+                    %(self.arrays[0].identifier, self.arrays[i].identifier)))
 
         if self.intermediates is not None:
+
+            spec = self.spec[self.arrays[0]].copy()
             for intermediate in self.intermediates:
-                self.provides(intermediate, common_spec[self.ch1])
+                self.provides(intermediate, spec)
 
-    def provide(self, request):
+    def _handle_intermediates(self, request):
 
-        batch = Batch()
-
-        # prepare: enlarge gt requested roi to be the same as the raw data
-        self.gt_spec = request[self.gt].roi.copy()
-        self.ch1_spec = request[self.ch1].roi.copy()
-        request[self.gt].roi = request[self.ch1].roi.copy()
-
-        if self.intermediates is not None:
-            # copy requested intermediates
-            intermediate_request = {}
-            for intermediate in self.intermediates:
-                intermediate_request[intermediate] = request[intermediate].copy()
+        # remove intermediates from request
+        for intermediate in self.intermediates:
+            if intermediate in request:
                 del request[intermediate]
 
-        # get volume A
-        a_request = np.random.choice(self.get_upstream_providers(), replace=False).request_batch(request)
-        a_ch1 = a_request[self.ch1].data
-        a_ch2 = a_request[self.ch2].data
-        a_gt = a_request[self.gt].data
+    def _enlarge_gt(self, request):
 
-        # get object from another volume
-        b_request = np.random.choice(self.get_upstream_providers(), replace=False).request_batch(request)
-        b_ch1 = b_request[self.ch1].data
-        b_ch2 = b_request[self.ch2].data
-        b_gt = b_request[self.gt].data
+        # enlarge roi for gt to be the same size as the raw data for mask generation
+        request[self.gt].roi = request[self.arrays[0]].roi.copy()
 
-        if self.intermediates is not None:
-            batch.arrays[ArrayKey('A_' + self.ch1.identifier)] = Array(
-                data=a_request[self.ch1].data, spec=a_request[self.ch1].spec.copy())
-            batch.arrays[ArrayKey('A_' + self.ch2.identifier)] = Array(
-                data=a_request[self.ch2].data, spec=a_request[self.ch2].spec.copy())
-            batch.arrays[ArrayKey('B_' + self.ch1.identifier)] = Array(
-                data=b_request[self.ch1].data, spec=b_request[self.ch1].spec.copy())
-            batch.arrays[ArrayKey('B_' + self.ch2.identifier)] = Array(
-                data=b_request[self.ch2].data, spec=b_request[self.ch2].spec.copy())
+    def prepare(self, request):
 
-        labels, counts = np.unique(b_gt, return_counts=True)
-        labels = np.delete(labels, 0)
-        counts = np.delete(counts, 0)
-        label = labels[np.argmax(counts)]
-        mask = b_gt == label
+        # save originally requested specs
+        self.gt_roi = request[self.gt].roi.copy()
+        self.array_roi = request[self.arrays[0]].roi.copy()
 
-        # position object
-        mask = self._position_object(a_gt, mask)
-
-        # create soft mask
-        soft_mask = np.zeros_like(mask, dtype='float32')
-        ndimage.gaussian_filter(mask.astype('float32'), sigma=self.smoothness, output=soft_mask, mode='nearest')
-        soft_mask /= np.max(soft_mask)
-        soft_mask = np.clip((soft_mask*1.2), 0, 1)
+        self._enlarge_gt(request)
 
         if self.intermediates is not None:
-            soft_mask_spec = a_request[self.ch1].spec.copy()
-            soft_mask_spec.dtype = np.float32
-            batch.arrays[ArrayKey('SOFT_MASK')] = Array(data=soft_mask, spec=soft_mask_spec)
+            self._handle_intermediates(request)
 
-        # paste object from b to a using convex combination
-        a_ch1 = soft_mask * b_ch1 + (1 - soft_mask) * a_ch1
-        a_ch2 = soft_mask * b_ch2 + (1 - soft_mask) * a_ch2
-        a_gt[mask] = np.max(a_gt) + 1
+    def process(self, batch, request):
 
-        a_gt = self._relabel(a_gt.astype(np.int32))
+        array_spec = self.spec[self.arrays[0]].copy()
+        array_spec.roi = request[self.arrays[0]].roi.copy()
+
+        # volume A is provided batch
+        a_arrays = [batch[array].data for array in self.arrays]
+        a_gt = batch[self.gt].data
+
+        # get volume B with additional request
+        if self.intermediates is not None:
+            self._handle_intermediates(request)
+
+        self._enlarge_gt(request)
+        b_batch = self.get_upstream_provider().request_batch(request)
+        b_arrays = [b_batch[array].data for array in self.arrays]
+        b_gt = b_batch[self.gt].data
+
+        # add original data of A and B to batch
+        if self.intermediates is not None:
+            for i, array in enumerate(self.arrays):
+                batch.arrays[ArrayKey('A_' + array.identifier)] = Array(data=a_arrays[i], spec=array_spec.copy())
+                batch.arrays[ArrayKey('B_' + array.identifier)] = Array(data=b_arrays[i], spec=array_spec.copy())
+
+        # create fused gt
+        fused_gt = a_gt.copy()
+        labels = np.unique(b_gt[b_gt > 0])
+
+        if 0 < self.num_blended_obj < len(labels):
+            labels = np.random.sample(labels, self.num_blended_obj)
+
+        fused_gt = self._relabel(fused_gt.astype(np.int32))
+        cnt = np.max(fused_gt) + 1
+        for label in labels:
+            fused_gt[b_gt == label] = cnt
+            cnt += 1
+        mask = fused_gt > (cnt - len(labels) - 1)
+
+        # create fused raw
+        fused = []
+        if self.alpha_blending_ref == 'intensity':
+
+            for i in range(len(self.arrays)):
+
+                b_mask = b_arrays[i].astype(np.float32) / np.max(b_arrays[i])
+                fused += [b_arrays[i] * b_mask + a_arrays[i] * (1 - b_mask), ]
+
+        elif self.alpha_blending_ref == 'gt':
+
+            # generate gt soft mask
+            mask = self._position_object(a_gt, mask)
+
+            # create soft mask
+            soft_mask = np.zeros_like(mask, dtype='float32')
+            ndimage.gaussian_filter(mask.astype('float32'), sigma=self.smoothness, output=soft_mask, mode='nearest')
+            soft_mask /= np.max(soft_mask)
+            soft_mask = np.clip((soft_mask * 2), 0, 1)
+
+            if self.intermediates is not None:
+                soft_mask_spec = array_spec.copy()
+                soft_mask_spec.dtype = np.float32
+                batch.arrays[ArrayKey('SOFT_MASK')] = Array(data=soft_mask, spec=soft_mask_spec)
+
+            for i in range(len(self.arrays)):
+
+                fused += [soft_mask * b_arrays[i] + (1 - soft_mask) * a_arrays[i], ]
+
+        else:
+            raise NotImplementedError("Unknown alpha blending reference %s." % self.alpha_blending_ref)
 
         gt_spec = self.spec[self.gt].copy()
         gt_spec.roi = request[self.gt].roi.copy()
 
-        ch1_spec = self.spec[self.ch1].copy()
-        ch1_spec.roi = request[self.ch1].roi.copy()
+        # return fused raw and gt volume
+        for i in range(len(self.arrays)):
+            batch.arrays[self.arrays[i]] = Array(data=fused[i].astype(array_spec.dtype), spec=array_spec)
 
-        ch2_spec = self.spec[self.ch2].copy()
-        ch2_spec.roi = request[self.ch2].roi.copy()
-
-        # return augmented raw and gt volume
-        batch.arrays[self.ch1] = Array(data=a_ch1.astype(ch1_spec.dtype), spec=ch1_spec)
-        batch.arrays[self.ch2] = Array(data=a_ch2.astype(ch2_spec.dtype), spec=ch2_spec)
-        batch.arrays[self.gt] = Array(data=a_gt.astype(gt_spec.dtype), spec=gt_spec).crop(self.gt_spec)
-
-        for points_key in a_request.points:
-            batch.points[points_key] = a_request.points[points_key]
+        batch.arrays[self.gt] = Array(data=fused_gt.astype(gt_spec.dtype), spec=gt_spec).crop(self.gt_roi)
 
         return batch
 
