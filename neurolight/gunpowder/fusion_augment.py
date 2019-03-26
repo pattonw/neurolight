3c1f4efb6705df9a085624f3255ffa5aee156168
maisli <maisl@kainmueller-ws2.int.janelia.org>
2019-03-26 00:59:22 -0400

    make fusion augment more gunpowdery

--- neurolight/gunpowder/fusion_augment.py
+++ neurolight/gunpowder/fusion_augment.py
@@ -6,178 +6,135 @@ from scipy import ndimage
 class FusionAugment(BatchFilter):
     """Combine foreground of one or more volumes with another using soft mask and convex combination.
         Args:
-            arrays (``list`` of :class:``ArrayKey``):
+            raw_base (:class:``ArrayKey``):
 
-                The intensity arrays to modify.
+                The intensity array for "base" volume.
 
-            gt (:class:``ArrayKey``):
+            raw_add (:class:``ArrayKey``):
 
-                The labeled array to use as mask to cut out foreground objects.
+                The intensity array for "add" volume.
 
-            alpha_blending_ref(``string``, optional):
+            labels_base (:class:``ArrayKey``):
 
-                Determines if alpha blending should be applied to intensities of volume B or gt mask of it.
+                The labeled array for "base" volume.
 
-            num_blended_obj (``int``):
+            labels_add (:class:``ArrayKey``):
 
-                The number of objects which should be used from volume B. Use 0 to copy all objects. Can only be \
-                applied to alpha_blending_ref gt.
+                The labeled array for "add" volume.
 
-            smoothness (``float``, optional):
+            raw_fused (:class:``ArrayKey``):
 
-                Set sigma for gaussian smoothing as size of soft mask around foreground object.
+                The intensity array for "fused" volume.
 
-            return_intermediate (``bool``, optional):
+            labels_fused (:class:``ArrayKey``):
 
-                If intermediate results should be returned (ch1 and ch2 of the two merged volumes (A and B) + soft mask)
+                The labeled array for "fused" volume.
+
+            blend_mode(``string``, optional):
+
+                One of "labels_mask" or "intensities". If "labels_mask" (the default), alpha blending is applied
+                to the labels mask of "add" volume. If "intensities", raw intensities of "add" volume are used.
+
+            blend_smoothness (``float``, optional):
+
+                Set sigma for gaussian smoothing of labels mask of "add" volume.
+
+            num_blended_objects (``int``):
+
+                The number of objects which should be used from "add" volume to copy it into "base" volume.
+                Use 0 to copy all objects. Can only be applied to blend mode "labels_mask".
     """
 
-    def __init__(self, arrays, gt, alpha_blending_ref='intensity', num_blended_obj=0, smoothness=3,
-                 return_intermediate=False):
+    def __init__(self, raw_base, raw_add, labels_base, labels_add, raw_fused, labels_fused,
+                      blend_mode='labels_mask', blend_smoothness=3, num_blended_objects=0):
 
-        if type(arrays) in [list, tuple]:
-            self.arrays = list(arrays)
-        else:
-            self.arrays = [arrays, ]
+        self.raw_base = raw_base
+        self.raw_add = raw_add
+        self.labels_base = labels_base
+        self.labels_add = labels_add
+        self.raw_fused = raw_fused
+        self.labels_fused = labels_fused
+        self.blend_mode = blend_mode
+        self.blend_smoothness = blend_smoothness
+        self.num_blended_obj = num_blended_objects
 
-        self.gt = gt
-        self.gt_roi = None
-        self.array_roi = None
-        self.alpha_blending_ref = alpha_blending_ref
-        self.num_blended_obj = num_blended_obj
-        self.smoothness = smoothness
-        self.intermediates = None
-
-        if return_intermediate:
-
-            self.intermediates = []
-
-            for array in self.arrays:
-                self.intermediates += [
-                    ArrayKey('A_' + array.identifier),
-                    ArrayKey('B_' + array.identifier),
-                ]
-            if self.alpha_blending_ref == 'gt':
-                self.intermediates += [ArrayKey('SOFT_MASK'),]
-
-        assert self.alpha_blending_ref in ['intensity', 'gt'], (
-                "Unknown alpha blending reference %s." % self.alpha_blending_ref)
+        assert self.blend_mode in ['intensity', 'labels_mask'], (
+                "Unknown blend mode %s." % self.blend_mode)
 
     def setup(self):
 
-        if len(self.arrays) > 1:
-            for i in range(1, len(self.arrays)):
-                assert(self.spec[self.arrays[0]] == self.spec[self.arrays[i]], (
-                    "Specs for %s and %s are not the same. Please check!"
-                    %(self.arrays[0].identifier, self.arrays[i].identifier)))
-
-        if self.intermediates is not None:
-
-            spec = self.spec[self.arrays[0]].copy()
-            for intermediate in self.intermediates:
-                self.provides(intermediate, spec)
-
-    def _handle_intermediates(self, request):
-
-        # remove intermediates from request
-        for intermediate in self.intermediates:
-            if intermediate in request:
-                del request[intermediate]
-
-    def _enlarge_gt(self, request):
-
-        # enlarge roi for gt to be the same size as the raw data for mask generation
-        request[self.gt].roi = request[self.arrays[0]].roi.copy()
+        self.provides(self.raw_fused, self.spec[self.raw_base].copy())
+        self.provides(self.labels_fused, self.spec[self.labels_base].copy())
 
     def prepare(self, request):
 
-        # save originally requested specs
-        self.gt_roi = request[self.gt].roi.copy()
-        self.array_roi = request[self.arrays[0]].roi.copy()
+        # add "base" and "add" volume to request
+        raw_fused_spec = request[self.raw_fused].copy()
+        # enlarge roi for labels to be the same size as the raw data for mask generation
+        labels_fused_spec = request[self.raw_fused].copy()
 
-        self._enlarge_gt(request)
-
-        if self.intermediates is not None:
-            self._handle_intermediates(request)
+        request.add(self.raw_base, raw_fused_spec.roi.get_shape())
+        request.add(self.raw_add, raw_fused_spec.roi.get_shape())
+        request.add(self.labels_base, labels_fused_spec.roi.get_shape())
+        request.add(self.labels_base, labels_fused_spec.roi.get_shape())
 
     def process(self, batch, request):
 
-        array_spec = self.spec[self.arrays[0]].copy()
-        array_spec.roi = request[self.arrays[0]].roi.copy()
+        # copy "base" volume to "fused"
+        raw_fused_array = batch[self.raw_base].data.copy()
+        raw_fused_spec = batch[self.raw_base].spec.copy()
+        labels_fused_array = batch[self.labels_base].data.copy()
+        labels_fused_spec = request[self.labels_fused].copy()
 
-        # volume A is provided batch
-        a_arrays = [batch[array].data for array in self.arrays]
-        a_gt = batch[self.gt].data
+        raw_add_array = batch[self.raw_add].data
+        labels_add_array = batch[self.labels_add].data
+        labels_add_spec = batch[self.labels_add].spec.copy()
 
-        # get volume B with additional request
-        if self.intermediates is not None:
-            self._handle_intermediates(request)
-
-        self._enlarge_gt(request)
-        b_batch = self.get_upstream_provider().request_batch(request)
-        b_arrays = [b_batch[array].data for array in self.arrays]
-        b_gt = b_batch[self.gt].data
-
-        # add original data of A and B to batch
-        if self.intermediates is not None:
-            for i, array in enumerate(self.arrays):
-                batch.arrays[ArrayKey('A_' + array.identifier)] = Array(data=a_arrays[i], spec=array_spec.copy())
-                batch.arrays[ArrayKey('B_' + array.identifier)] = Array(data=b_arrays[i], spec=array_spec.copy())
-
-        # create fused gt
-        fused_gt = a_gt.copy()
-        labels = np.unique(b_gt[b_gt > 0])
+        # fuse labels, create labels_mask of "add" volume
+        labels = np.unique(labels_add_array)
+        if 0 in labels:
+            np.delete(labels, 0)
 
         if 0 < self.num_blended_obj < len(labels):
             labels = np.random.sample(labels, self.num_blended_obj)
 
-        fused_gt = self._relabel(fused_gt.astype(np.int32))
-        cnt = np.max(fused_gt) + 1
+        labels_fused_array = self._relabel(labels_fused_array.astype(np.int32))
+        labels_fused_mask = labels_fused_array > 0
+        mask = np.zeros_like(labels_fused_array, dtype=bool)
+        cnt = np.max(labels_fused_array) + 1
+
         for label in labels:
-            fused_gt[b_gt == label] = cnt
+            label_mask = labels_add_array == label
+            overlap = np.logical_and(labels_fused_mask, label_mask)
+            mask[label_mask] = True
+            labels_fused_array[label_mask] = cnt    # todo: position object randomly or with specified overlap/distance
+            labels_fused_array[overlap] = 0     # set label 0 for overlapping neurons
             cnt += 1
-        mask = fused_gt > (cnt - len(labels) - 1)
 
-        # create fused raw
-        fused = []
-        if self.alpha_blending_ref == 'intensity':
+        # fuse raw
+        if self.blend_mode == 'intensity':
 
-            for i in range(len(self.arrays)):
+            add_mask = raw_add_array.astype(np.float32) / np.max(raw_add_array)
+            raw_fused_array = add_mask * raw_add_array + (1 - add_mask) * raw_fused_array
 
-                b_mask = b_arrays[i].astype(np.float32) / np.max(b_arrays[i])
-                fused += [b_arrays[i] * b_mask + a_arrays[i] * (1 - b_mask), ]
-
-        elif self.alpha_blending_ref == 'gt':
-
-            # generate gt soft mask
-            mask = self._position_object(a_gt, mask)
+        elif self.blend_mode == 'labels_mask':
 
             # create soft mask
             soft_mask = np.zeros_like(mask, dtype='float32')
-            ndimage.gaussian_filter(mask.astype('float32'), sigma=self.smoothness, output=soft_mask, mode='nearest')
+            ndimage.gaussian_filter(mask.astype('float32'), sigma=self.blend_smoothness, output=soft_mask,
+                                    mode='nearest')
             soft_mask /= np.max(soft_mask)
             soft_mask = np.clip((soft_mask * 2), 0, 1)
 
-            if self.intermediates is not None:
-                soft_mask_spec = array_spec.copy()
-                soft_mask_spec.dtype = np.float32
-                batch.arrays[ArrayKey('SOFT_MASK')] = Array(data=soft_mask, spec=soft_mask_spec)
-
-            for i in range(len(self.arrays)):
-
-                fused += [soft_mask * b_arrays[i] + (1 - soft_mask) * a_arrays[i], ]
+            raw_fused_array = soft_mask * raw_add_array + (1 - soft_mask) * raw_fused_array
 
         else:
-            raise NotImplementedError("Unknown alpha blending reference %s." % self.alpha_blending_ref)
+            raise NotImplementedError("Unknown blend mode %s." % self.blend_mode)
 
-        gt_spec = self.spec[self.gt].copy()
-        gt_spec.roi = request[self.gt].roi.copy()
-
-        # return fused raw and gt volume
-        for i in range(len(self.arrays)):
-            batch.arrays[self.arrays[i]] = Array(data=fused[i].astype(array_spec.dtype), spec=array_spec)
-
-        batch.arrays[self.gt] = Array(data=fused_gt.astype(gt_spec.dtype), spec=gt_spec).crop(self.gt_roi)
+        # return raw and labels for "fused" volume
+        batch.arrays[self.raw_fused] = Array(data=raw_fused_array.astype(raw_fused_spec.dtype), spec=raw_fused_spec)
+        batch.arrays[self.labels_fused] = Array(data=labels_fused_array.astype(labels_fused_spec.dtype),
+                                                spec=labels_add_spec).crop(labels_fused_spec.roi)
 
         return batch
 
@@ -194,25 +151,3 @@ class FusionAugment(BatchFilter):
         values_map[old_values] = new_values
 
         return values_map[a]
-
-    def _get_overlap(self, a, b):
-
-        return np.sum(np.logical_and(a > 0, b > 0))
-
-    def _get_distance_to_next_object(self, a, b):
-
-        fg = a > 0
-        dt_next_object = np.zeros_like(a, dtype='float32')
-        object_mask = b > 0
-        dt_next_object[object_mask] = ndimage.distance_transform_edt(np.logical_not(fg))[object_mask]
-
-        return dt_next_object
-
-    def _position_object(self, gt, mask):
-
-        # todo: position object randomly or with certain overlap/min_distance
-        overlap = self._get_overlap(gt, mask)
-        min_distance, counts = np.unique(self._get_distance_to_next_object(gt, mask), return_counts=True)
-        print('overlap: ', overlap, ' min_distance: ', np.min(np.delete(min_distance, 0)))
-
-        return mask
