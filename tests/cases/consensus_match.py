95e22df17b936760eeec64692db1faccbacb434e
pattonw <pattonw@hhmi.org>
2019-09-25 14:51:49 -0400

    formatting with black
    making sure everything is formatted consistently

--- tests/cases/consensus_match.py
+++ tests/cases/consensus_match.py
@@ -3,8 +3,8 @@ import neurolight as nl
 import networkx as nx
 import numpy as np
 
-class ConsensusMatchTest(unittest.TestCase):
 
+class ConsensusMatchTest(unittest.TestCase):
     def test_simple(self):
 
         # consensus graph:
@@ -15,36 +15,36 @@ class ConsensusMatchTest(unittest.TestCase):
         #
         # o--o--o--o--o
         consensus = nx.Graph()
-        consensus.add_nodes_from([
-            (1, {'location': np.array([0, 0, 0])}),
-            (2, {'location': np.array([0, 0, 10])}),
-            (3, {'location': np.array([0, 0, 20])})])
-        consensus.add_edges_from([
-            (1, 2),
-            (2, 3)])
+        consensus.add_nodes_from(
+            [
+                (1, {"location": np.array([0, 0, 0])}),
+                (2, {"location": np.array([0, 0, 10])}),
+                (3, {"location": np.array([0, 0, 20])}),
+            ]
+        )
+        consensus.add_edges_from([(1, 2), (2, 3)])
 
         skeleton = nx.Graph()
-        skeleton.add_nodes_from([
-            (1, {'location': np.array([0, 0, 0])}),
-            (2, {'location': np.array([0, 0, 5])}),
-            (3, {'location': np.array([0, 0, 10])}),
-            (4, {'location': np.array([0, 0, 15])}),
-            (5, {'location': np.array([0, 0, 20])})])
-        skeleton.add_edges_from([
-            (1, 2),
-            (2, 3),
-            (3, 4),
-            (4, 5)])
+        skeleton.add_nodes_from(
+            [
+                (1, {"location": np.array([0, 0, 0])}),
+                (2, {"location": np.array([0, 0, 5])}),
+                (3, {"location": np.array([0, 0, 10])}),
+                (4, {"location": np.array([0, 0, 15])}),
+                (5, {"location": np.array([0, 0, 20])}),
+            ]
+        )
+        skeleton.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5)])
 
         nl.match_graph_to_tree(
             skeleton,
             consensus,
             match_attribute='matched_edge')
 
-        self.assertEqual(skeleton.edges[(1, 2)]['matched_edge'] == (1, 2))
-        self.assertEqual(skeleton.edges[(2, 3)]['matched_edge'] == (1, 2))
-        self.assertEqual(skeleton.edges[(3, 4)]['matched_edge'] == (2, 3))
-        self.assertEqual(skeleton.edges[(4, 5)]['matched_edge'] == (2, 3))
+        self.assertEqual(skeleton.edges[(1, 2)]["matched_edge"] == (1, 2))
+        self.assertEqual(skeleton.edges[(2, 3)]["matched_edge"] == (1, 2))
+        self.assertEqual(skeleton.edges[(3, 4)]["matched_edge"] == (2, 3))
+        self.assertEqual(skeleton.edges[(4, 5)]["matched_edge"] == (2, 3))
 
     def test_simple_4_way(self):
 
@@ -88,53 +88,74 @@ class ConsensusMatchTest(unittest.TestCase):
         # h should map to C-XC
 
         consensus = nx.Graph()
-        consensus.add_nodes_from([
-            ('D', {'location': np.array([0, 0, 0])}),
-            ('B', {'location': np.array([0, 0, 20])}),
-            ('X', {'location': np.array([0, 0, 10])}),
-            ('A', {'location': np.array([0, -10, 10])}),
-            ('C', {'location': np.array([0, 10, 10])})])
-        consensus.add_edges_from([
-            ('A', 'X'),
-            ('B', 'X'),
-            ('C', 'X'),
-            ('D', 'X')])
+        consensus.add_nodes_from(
+            [
+                ("D", {"location": np.array([0, 0, 0])}),
+                ("B", {"location": np.array([0, 0, 20])}),
+                ("X", {"location": np.array([0, 0, 10])}),
+                ("A", {"location": np.array([0, -10, 10])}),
+                ("C", {"location": np.array([0, 10, 10])}),
+            ]
+        )
+        consensus.add_edges_from([("A", "X"), ("B", "X"), ("C", "X"), ("D", "X")])
 
         skeleton = nx.Graph()
-        skeleton.add_nodes_from([
-            ('c', {'location': np.array([0, 0, 0])}),
-            ('d', {'location': np.array([0, 0, 5])}),
-            ('g', {'location': np.array([0, 0, 20])}),
-            ('f', {'location': np.array([0, 0, 15])}),
-            ('e', {'location': np.array([0, 0, 10])}),
-            ('b', {'location': np.array([0, -5, 5])}),
-            ('a', {'location': np.array([0, -10, 5])}),
-            ('h', {'location': np.array([0, 5, 15])}),
-            ('i', {'location': np.array([0, 10, 15])})])
-        skeleton.add_edges_from([
-            ('a', 'b'),
-            ('b', 'd'),
-            ('c', 'd'),
-            ('d', 'e'),
-            ('e', 'f'),
-            ('f', 'g'),
-            ('f', 'h'),
-            ('h', 'i')])
+        skeleton.add_nodes_from(
+            [
+                ("c", {"location": np.array([0, 0, 0])}),
+                ("d", {"location": np.array([0, 0, 5])}),
+                ("g", {"location": np.array([0, 0, 20])}),
+                ("f", {"location": np.array([0, 0, 15])}),
+                ("e", {"location": np.array([0, 0, 10])}),
+                ("b", {"location": np.array([0, -5, 5])}),
+                ("a", {"location": np.array([0, -10, 5])}),
+                ("h", {"location": np.array([0, 5, 15])}),
+                ("i", {"location": np.array([0, 10, 15])}),
+            ]
+        )
+        skeleton.add_edges_from(
+            [
+                ("a", "b"),
+                ("b", "d"),
+                ("c", "d"),
+                ("d", "e"),
+                ("e", "f"),
+                ("f", "g"),
+                ("f", "h"),
+                ("h", "i"),
+            ]
+        )
 
         nl.match_graph_to_tree(
             skeleton,
             consensus,
             match_attribute='matched_edge')
 
-        self.assertEqual(skeleton.edges[('a', 'b')]['matched_edge'] == ('A', ('X', 'A')))
-        self.assertEqual(skeleton.edges[('c', 'd')]['matched_edge'] == ('D', ('X', 'D')))
-        self.assertEqual(skeleton.edges[('f', 'g')]['matched_edge'] == ('B', ('X', 'B')))
-        self.assertEqual(skeleton.edges[('h', 'i')]['matched_edge'] == ('C', ('X', 'C')))
+        self.assertEqual(
+            skeleton.edges[("a", "b")]["matched_edge"] == ("A", ("X", "A"))
+        )
+        self.assertEqual(
+            skeleton.edges[("c", "d")]["matched_edge"] == ("D", ("X", "D"))
+        )
+        self.assertEqual(
+            skeleton.edges[("f", "g")]["matched_edge"] == ("B", ("X", "B"))
+        )
+        self.assertEqual(
+            skeleton.edges[("h", "i")]["matched_edge"] == ("C", ("X", "C"))
+        )
 
-        self.assertEqual(skeleton.edges[('b', 'd')]['matched_edge'], (('X', 'A'), ('X', 'D')))
-        self.assertEqual(skeleton.edges[('d', 'e')]['matched_edge'], (('X', 'D'), ('X', 'B')))
-        self.assertEqual(skeleton.edges[('e', 'f')]['matched_edge'], (('X', 'D'), ('X', 'B')))
-        self.assertEqual(skeleton.edges[('f', 'h')]['matched_edge'], (('X', 'B'), ('X', 'C')))
+        self.assertEqual(
+            skeleton.edges[("b", "d")]["matched_edge"], (("X", "A"), ("X", "D"))
+        )
+        self.assertEqual(
+            skeleton.edges[("d", "e")]["matched_edge"], (("X", "D"), ("X", "B"))
+        )
+        self.assertEqual(
+            skeleton.edges[("e", "f")]["matched_edge"], (("X", "D"), ("X", "B"))
+        )
+        self.assertEqual(
+            skeleton.edges[("f", "h")]["matched_edge"], (("X", "B"), ("X", "C"))
+        )
 
     def test_confounding_chain(self):
 
@@ -153,42 +174,48 @@ class ConsensusMatchTest(unittest.TestCase):
         # None to c-f, and BC to f-h, and h-i to reduce cost
 
         consensus = nx.Graph()
-        consensus.add_nodes_from([
-            ('A', {'location': np.array([0, 0, 0])}),
-            ('B', {'location': np.array([0, 0, 10])}),
-            ('C', {'location': np.array([0, 0, 20])})])
-        consensus.add_edges_from([
-            ('A', 'B'),
-            ('B', 'C')])
+        consensus.add_nodes_from(
+            [
+                ("A", {"location": np.array([0, 0, 0])}),
+                ("B", {"location": np.array([0, 0, 10])}),
+                ("C", {"location": np.array([0, 0, 20])}),
+            ]
+        )
+        consensus.add_edges_from([("A", "B"), ("B", "C")])
 
         skeleton = nx.Graph()
-        skeleton.add_nodes_from([
-            ('a', {'location': np.array([0, 0, 0])}),
-            ('b', {'location': np.array([0, 0, 5])}),
-            ('c', {'location': np.array([0, 0, 10])}),
-            ('d', {'location': np.array([0, 0, 15])}),
-            ('e', {'location': np.array([0, 0, 20])}),
-            ('f', {'location': np.array([0, -1, 10])}),
-            ('h', {'location': np.array([0, -1, 15])}),
-            ('i', {'location': np.array([0, -1, 20])})])
-        skeleton.add_edges_from([
-            ('a', 'b'),
-            ('b', 'c'),
-            ('c', 'd'),
-            ('d', 'e'),
-            ('c', 'f'),
-            ('f', 'h'),
-            ('h', 'i')])
+        skeleton.add_nodes_from(
+            [
+                ("a", {"location": np.array([0, 0, 0])}),
+                ("b", {"location": np.array([0, 0, 5])}),
+                ("c", {"location": np.array([0, 0, 10])}),
+                ("d", {"location": np.array([0, 0, 15])}),
+                ("e", {"location": np.array([0, 0, 20])}),
+                ("f", {"location": np.array([0, -1, 10])}),
+                ("h", {"location": np.array([0, -1, 15])}),
+                ("i", {"location": np.array([0, -1, 20])}),
+            ]
+        )
+        skeleton.add_edges_from(
+            [
+                ("a", "b"),
+                ("b", "c"),
+                ("c", "d"),
+                ("d", "e"),
+                ("c", "f"),
+                ("f", "h"),
+                ("h", "i"),
+            ]
+        )
 
         nl.match_graph_to_tree(
             skeleton,
             consensus,
-            match_attribute='matched_edge')
+        self.assertEqual(skeleton.edges[("a", "b")]["matched_edge"], ("A", "B"))
+        self.assertEqual(skeleton.edges[("b", "c")]["matched_edge"], ("A", "B"))
+        self.assertEqual(skeleton.edges[("c", "d")]["matched_edge"], ("B", "C"))
+        self.assertEqual(skeleton.edges[("d", "e")]["matched_edge"], ("B", "C"))
+        self.assertEqual(skeleton.edges[("c", "f")]["matched_edge"], None)
+        self.assertEqual(skeleton.edges[("f", "h")]["matched_edge"], None)
+        self.assertEqual(skeleton.edges[("h", "i")]["matched_edge"], None)
 
-        self.assertEqual(skeleton.edges[('a', 'b')]['matched_edge'], ('A', 'B'))
-        self.assertEqual(skeleton.edges[('b', 'c')]['matched_edge'], ('A', 'B'))
-        self.assertEqual(skeleton.edges[('c', 'd')]['matched_edge'], ('B', 'C'))
-        self.assertEqual(skeleton.edges[('d', 'e')]['matched_edge'], ('B', 'C'))
-        self.assertEqual(skeleton.edges[('c', 'f')]['matched_edge'], None)
-        self.assertEqual(skeleton.edges[('f', 'h')]['matched_edge'], None)
-        self.assertEqual(skeleton.edges[('h', 'i')]['matched_edge'], None)
