4e52a949cf7a69b4e957b637731231d455617185
pattonw <pattonw@hhmi.org>
2019-10-01 11:27:34 -0400

    Add test for confounding loop

--- tests/cases/consensus_match.py
+++ tests/cases/consensus_match.py
@@ -208,3 +208,72 @@ class ConsensusMatchTest(unittest.TestCase):
         self.assertEqual(skeleton.edges[("f", "h")].get("matched_edge"), None)
         self.assertEqual(skeleton.edges[("h", "i")].get("matched_edge"), None)
 
+
+    def test_confounding_loop(self):
+
+        # consensus graph:
+        #
+        # A---->B---->C
+        #
+        #
+        # skeleton graph:
+        #
+        #  a--b--c--d--e
+        #
+        #        f--h
+        #        | /
+        #        i
+        #
+        # the optimal matching should not cheat and assign
+        # None to c-f, and BC to f-h, and h-i to reduce cost
+
+        consensus = nx.DiGraph()
+        consensus.add_nodes_from(
+            [
+                ("A", {"location": np.array([0, 0, 0])}),
+                ("B", {"location": np.array([0, 0, 10])}),
+                ("C", {"location": np.array([0, 0, 20])}),
+            ]
+        )
+        consensus.add_edges_from([("A", "B"), ("B", "C")])
+
+        skeleton = nx.Graph()
+        skeleton.add_nodes_from(
+            [
+                ("a", {"location": np.array([0, 0, 0])}),
+                ("b", {"location": np.array([0, 0, 5])}),
+                ("c", {"location": np.array([0, 0, 10])}),
+                ("d", {"location": np.array([0, 0, 15])}),
+                ("e", {"location": np.array([0, 0, 20])}),
+                ("f", {"location": np.array([0, -1, 10])}),
+                ("h", {"location": np.array([0, -1, 15])}),
+                ("i", {"location": np.array([0, -2, 10])}),
+            ]
+        )
+        skeleton.add_edges_from(
+            [
+                ("a", "b"),
+                ("b", "c"),
+                ("c", "d"),
+                ("d", "e"),
+                ("f", "h"),
+                ("f", "i"),
+                ("h", "i"),
+            ]
+        )
+
+        nl.match_graph_to_tree(
+            skeleton,
+            consensus,
+            match_distance_threshold=100,
+            match_attribute="matched_edge",
+        )
+
+        self.assertEqual(skeleton.edges[("a", "b")]["matched_edge"], ("A", "B"))
+        self.assertEqual(skeleton.edges[("b", "c")]["matched_edge"], ("A", "B"))
+        self.assertEqual(skeleton.edges[("c", "d")]["matched_edge"], ("B", "C"))
+        self.assertEqual(skeleton.edges[("d", "e")]["matched_edge"], ("B", "C"))
+        self.assertEqual(skeleton.edges[("f", "h")].get("matched_edge"), None)
+        self.assertEqual(skeleton.edges[("f", "i")].get("matched_edge"), None)
+        self.assertEqual(skeleton.edges[("h", "i")].get("matched_edge"), None)
+
