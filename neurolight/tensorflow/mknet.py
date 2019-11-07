c5f9ced18de7a5dc105d4c797227d639136e39ad
pattonw <pattonw@hhmi.org>
2019-11-07 14:39:21 -0500

    better variable names in mknet

--- neurolight/tensorflow/mknet.py
+++ neurolight/tensorflow/mknet.py
@@ -4,41 +4,46 @@ from funlib.learn.tensorflow.models import unet, conv_pass
 import numpy as np
 
 from typing import Dict, Any
+from pathlib import Path
 
 
-def mknet(config: Dict[str, Any]):
+def mknet(config: Dict[str, Any], output_path: Path = Path()):
 
-    INPUT_SHAPE = tuple(config["INPUT_SHAPE"])
-    EMBEDDING_DIMS = config["EMBEDDING_DIMS"]
-    NUM_FMAPS = config["NUM_FMAPS"]
-    FMAP_INC_FACTORS = config["FMAP_INC_FACTORS"]
+    input_shape = (2,) + tuple(config["INPUT_SHAPE"])
+    embedding_dims = config["EMBEDDING_DIMS"]
+    num_fmaps_embedding = config["NUM_FMAPS_EMBEDDING"]
+    fmap_inc_factors_embedding = config["FMAP_INC_FACTORS_EMBEDDING"]
+    num_fmaps_foreground = config["NUM_FMAPS_FOREGROUND"]
+    fmap_inc_factors_foreground = config["FMAP_INC_FACTORS_FOREGROUND"]
+    downsample_factors = config["DOWNSAMPLE_FACTORS"]
+    kernel_size_up = config["KERNEL_SIZE_UP"]
 
-    raw = tf.placeholder(tf.float32, shape=INPUT_SHAPE)
-    raw_batched = tf.reshape(raw, (1, 1) + INPUT_SHAPE)
+    raw = tf.placeholder(tf.float32, shape=input_shape, name="raw_input")
+    raw_batched = tf.reshape(raw, (1,) + input_shape)
 
     with tf.variable_scope("embedding"):
         embedding_unet = unet(
             raw_batched,
-            num_fmaps=NUM_FMAPS,
-            fmap_inc_factors=FMAP_INC_FACTORS,
-            downsample_factors=[[2, 2], [2, 2], [2, 2]],
-            kernel_size_up=[[3], [3], [3]],
+            num_fmaps=num_fmaps_embedding,
+            fmap_inc_factors=fmap_inc_factors_embedding,
+            downsample_factors=downsample_factors,
+            kernel_size_up=kernel_size_up,
             constant_upsample=True,
         )
     with tf.variable_scope("fg"):
         fg_unet = unet(
             raw_batched,
-            num_fmaps=6,
-            fmap_inc_factors=3,
-            downsample_factors=[[2, 2], [2, 2], [2, 2]],
-            kernel_size_up=[[3], [3], [3]],
+            num_fmaps=num_fmaps_foreground,
+            fmap_inc_factors=fmap_inc_factors_foreground,
+            downsample_factors=downsample_factors,
+            kernel_size_up=kernel_size_up,
             constant_upsample=True,
         )
 
     embedding_batched = conv_pass(
         embedding_unet[0],
         kernel_sizes=[1],
-        num_fmaps=EMBEDDING_DIMS,
+        num_fmaps=embedding_dims,
         activation=None,
         name="embedding",
     )
@@ -55,20 +60,18 @@ def mknet(config: Dict[str, Any]):
         output_shape_batched[2:]
     )  # strip the batch and channel dimension
 
-    assert all(
-        np.isclose(np.array(output_shape), np.array(config["OUTPUT_SHAPE"]))
-    )
+    assert all(np.isclose(np.array(output_shape), np.array(config["OUTPUT_SHAPE"])))
 
-    embedding = tf.reshape(embedding_scaled, (EMBEDDING_DIMS,) + output_shape)
+    embedding = tf.reshape(embedding_scaled, (embedding_dims,) + output_shape)
     fg = tf.reshape(fg_batched[0], output_shape)
-    gt_labels = tf.placeholder(tf.int64, shape=output_shape)
+    gt_labels = tf.placeholder(tf.int64, shape=output_shape, name="gt_labels")
 
-    tf.train.export_meta_graph(filename="train_net.meta")
+    tf.train.export_meta_graph(filename=output_path / "train_net.meta")
     names = {
         "raw": raw.name,
         "embedding": embedding.name,
         "fg": fg.name,
         "gt_labels": gt_labels.name,
     }
-    with open("tensor_names.json", "w") as f:
+    with (output_path / "tensor_names.json").open("w") as f:
         json.dump(names, f)
