8427bc2b31bdc8040514aee1cad9e68819b1a592
pattonw <pattonw@hhmi.org>
2019-11-21 15:30:48 -0500

    move mouselight specific parts of the funlib.match package here
    This includes calculating the matching cost between points since people
    may not want to use euclidean distance or include penalties.
    Also includes preprocessing of the graph.

---
+++ neurolight/match/costs.py
@@ -0,0 +1,150 @@
+import numpy as np
+from scipy.spatial import cKDTree
+import rtree
+import networkx as nx
+
+import itertools
+from typing import List, Hashable, Tuple
+
+Node = Hashable
+Edge = Tuple[Node, Node]
+
+
+def get_costs(
+    graph: nx.Graph,
+    tree: nx.DiGraph,
+    location_attr: str,
+    penalty_attr: str,
+    node_match_threshold: float,
+    edge_match_threshold: float,
+    node_balance: float,
+) -> Tuple[List[Tuple[Node, Node, float]], List[Tuple[Edge, Edge, float]]]:
+    graph_kd, graph_kd_ids, tree_kd, tree_kd_ids = initialize_kdtrees(
+        graph, tree, location_attr
+    )
+    tree_rtree = initialize_rtree(tree, location_attr)
+
+    node_matchings = []
+
+    close_enough = graph_kd.query_ball_tree(tree_kd, node_match_threshold)
+    for i, close_nodes in enumerate(close_enough):
+        for j in close_nodes:
+            graph_n = graph_kd_ids[i]
+            tree_n = tree_kd_ids[j]
+            distance = np.linalg.norm(
+                graph.nodes[graph_n][location_attr] - tree.nodes[tree_n][location_attr]
+            )
+            penalty = graph.nodes[graph_n].get(penalty_attr, 0) + tree.nodes[
+                tree_n
+            ].get(penalty_attr, 0)
+            node_matchings.append(
+                (graph_n, tree_n, node_cost(distance, penalty, node_balance))
+            )
+
+    edge_matchings = []
+    for graph_e in graph.edges():
+
+        possible_tree_edges = tree_edge_query(
+            graph.nodes[graph_e[0]][location_attr],
+            graph.nodes[graph_e[1]][location_attr],
+            edge_match_threshold,
+            tree_rtree,
+            tree,
+            location_attr,
+        )
+
+        for tree_e in possible_tree_edges:
+            distance = edge_dist(
+                graph.nodes[graph_e[0]][location_attr],
+                graph.nodes[graph_e[1]][location_attr],
+                tree.nodes[tree_e[0]][location_attr],
+                tree.nodes[tree_e[1]][location_attr],
+            )
+            penalty = graph.edges[graph_e].get(penalty_attr, 0) + tree.edges[
+                tree_e
+            ].get(penalty_attr, 0)
+            edge_matchings.append((graph_e, tree_e, edge_cost(distance, penalty)))
+
+    return node_matchings, edge_matchings
+
+
+def initialize_rtree(tree, location_attr):
+    p = rtree.index.Property()
+    p.dimension = 3
+    tree_rtree = rtree.index.Index(properties=p)
+    for i, (u, v) in enumerate(tree.edges()):
+        u_loc = tree.nodes[u][location_attr]
+        v_loc = tree.nodes[v][location_attr]
+        mins = np.min(np.array([u_loc, v_loc]), axis=0)
+        maxs = np.max(np.array([u_loc, v_loc]), axis=0)
+        box = tuple(x for x in itertools.chain(mins.tolist(), maxs.tolist()))
+        tree_rtree.insert(i, box, obj=(u, v))
+
+    return tree_rtree
+
+
+def initialize_kdtrees(graph: nx.Graph, tree: nx.DiGraph, location_attr: str):
+    tree_kd_ids, tree_node_attrs = [list(x) for x in zip(*tree.nodes.items())]
+    print(tree_node_attrs)
+    tree_kd = cKDTree([attrs[location_attr] for attrs in tree_node_attrs])
+
+    graph_kd_ids, graph_node_attrs = [list(x) for x in zip(*graph.nodes.items())]
+    print(graph_kd_ids, graph_node_attrs)
+    graph_kd = cKDTree([attrs[location_attr] for attrs in graph_node_attrs])
+
+    return graph_kd, graph_kd_ids, tree_kd, tree_kd_ids
+
+
+def node_cost(distance: float, penalty: float, node_balance: float) -> float:
+    return node_balance * (distance + penalty)
+
+
+def tree_edge_query(
+    u_loc: np.ndarray, v_loc: np.ndarray, radius: float, tree_rtree, tree, location_attr
+):
+    # r tree only stores bounding boxes of lines, so we have to retrieve all
+    # potential edges, and then filter them based on a line specific distance
+    # calculation
+    rect = tuple(
+        x
+        for x in itertools.chain(
+            (np.min(np.array([u_loc, v_loc]), axis=0) - radius).tolist(),
+            (np.min(np.array([u_loc, v_loc]), axis=0) + radius).tolist(),
+        )
+    )
+    possible_tree_edges = [
+        x.object for x in tree_rtree.intersection(rect, objects=True)
+    ]
+    # line distances
+    for x, y in possible_tree_edges:
+        dist = edge_dist(
+            u_loc, v_loc, tree.nodes[x][location_attr], tree.nodes[y][location_attr]
+        )
+        if dist < radius:
+            yield (x, y)
+
+
+def edge_dist(
+    u_loc: np.ndarray, v_loc: np.ndarray, x_loc: np.ndarray, y_loc: np.ndarray
+) -> float:
+    distance = (
+        point_to_edge_dist(u_loc, x_loc, y_loc)
+        + point_to_edge_dist(v_loc, x_loc, y_loc)
+    ) / 2
+    return distance
+
+
+def point_to_edge_dist(
+    center: np.ndarray, u_loc: np.ndarray, v_loc: np.ndarray
+) -> float:
+    slope = v_loc - u_loc
+    edge_mag = np.linalg.norm(slope)
+    if np.isclose(edge_mag, 0):
+        return np.linalg.norm(u_loc - center)
+    frac = np.clip(np.dot(center - u_loc, slope) / np.dot(slope, slope), 0, 1)
+    min_dist = np.linalg.norm(frac * slope + u_loc - center)
+    return min_dist
+
+
+def edge_cost(distance, penalty) -> float:
+    return distance + penalty
---
+++ neurolight/match/preprocess.py
@@ -0,0 +1,66 @@
+import networkx as nx
+import numpy as np
+from scipy.spatial import cKDTree
+
+from collections import Counter
+import itertools
+from typing import Dict
+
+
+def mouselight_preprocessing(
+    graph: nx.DiGraph(), min_dist: float, voxel_size=[10, 3, 3]
+):
+    """
+    Preprocessing for mouselight skeletonizations s.t. they can be matched
+    to mouselight consensus neurons.
+
+    Common problems seen in skeletonizations are:
+    
+    Gaps:
+    A------------------------------B
+    a---b---c---d--e      f--g--h--i
+
+    Crossovers:
+            A
+            |                       a
+            |                       |
+    B-------C-------D   vs      b---c-d----e
+            |                         |
+            |                         f
+            E
+
+    Solution:
+    Remove nodes of degree > 1, and then for all pairwise connected components,
+    create edges between all nodes in component a to all nodes in component b
+    if the edge is below `min_dist`.
+    """
+
+    temp = graph.to_undirected()
+
+    temp.remove_nodes_from(filter(lambda x: temp.degree(x) > 2, list(temp.nodes)))
+
+    wccs = list(list(x) for x in nx.connected_components(temp))
+    if len(wccs) < 2:
+        return
+
+    spatial_wccs = [cKDTree([temp.nodes[x]["location"] for x in wcc]) for wcc in wccs]
+    for (ind_a, spatial_a), (ind_b, spatial_b) in itertools.combinations(
+        enumerate(spatial_wccs), 2
+    ):
+        for node_a_index, closest_nodes in itertools.chain(
+            *zip(enumerate(spatial_a.query_ball_tree(spatial_b, min_dist)))
+        ):
+            for node_b_index in closest_nodes:
+                node_a = wccs[ind_a][node_a_index]
+                node_b = wccs[ind_b][node_b_index]
+                edge_len = np.linalg.norm(
+                    temp.nodes[node_a]["location"] - temp.nodes[node_b]["location"]
+                )
+                if edge_len < min_dist:
+                    # Add extra penalties to added edges to minimize cable length
+                    # assigned to ambiguous ground truth.
+                    graph.add_edge(
+                        node_a,
+                        node_b,
+                        penalty=((edge_len + 1) ** 2) / np.linalg.norm(voxel_size),
+                    )
