b307311d63cb486bee50a3d640ef01022122e35b
pattonw <pattonw@hhmi.org>
2019-12-06 10:32:38 -0500

    Add a test for the foreground module

---
+++ tests/networks/test_foreground_unet.py
@@ -0,0 +1,29 @@
+import torch
+
+from neurolight.networks.pytorch.fg_unet import ForegroundUnet
+
+
+def test_emb_unet():
+    config = {
+        "NETWORK_NAME": "fg_net",
+        "INPUT_SHAPE": [80, 256, 256],
+        "OUTPUT_SHAPE": [32, 120, 120],
+        "NUM_FMAPS_FOREGROUND": 3,
+        "FMAP_INC_FACTORS_FOREGROUND": 2,
+        "DOWNSAMPLE_FACTORS": [(1, 3, 3), (2, 2, 2), (2, 2, 2)],
+        "KERNEL_SIZE_UP": [
+            [[3, 3, 3], [3, 3, 3]],
+            [[3, 3, 3], [3, 3, 3]],
+            [[3, 3, 3], [3, 3, 3]],
+            [[3, 3, 3], [3, 3, 3]],
+        ],
+        "VOXEL_SIZE": [1, 1, 1],
+    }
+    denoiser = ForegroundUnet(config)
+
+    raw = torch.rand(1, 2, 80, 256, 256)
+    foreground = denoiser(raw)
+    assert foreground.shape == (1, 1, 32, 120, 120)
+    assert foreground.min() > 0
+    assert foreground.max() < 1
+
