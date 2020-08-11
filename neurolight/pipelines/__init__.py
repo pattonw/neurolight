823fa9147b4d4e642f9e03b15ff9ec8f23aaaca6
pattonw <pattonw@hhmi.org>
2020-08-11 08:44:13 -0400

    update configs

--- neurolight/pipelines/__init__.py
+++ neurolight/pipelines/__init__.py
@@ -5,7 +5,9 @@ from pathlib import Path
 
 cwd = Path(__file__).parent
 config_file = cwd / "default_config.json"
+eval_config_file = cwd / "evaluation_config.json"
 
 DEFAULT_CONFIG = json.load(config_file.open("r"))
+DEFAULT_EVAL_CONFIG = json.load(eval_config_file.open("r"))
 
-__all__ = ["embedding_pipeline", "foreground_pipeline", "DEFAULT_CONFIG"]
+__all__ = ["embedding_pipeline", "foreground_pipeline", "DEFAULT_CONFIG", "DEFAULT_EVAL_CONFIG"]
